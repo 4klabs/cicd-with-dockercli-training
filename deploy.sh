8215688272dbee8f527bd6355a948951d2ef78c7
Mateus Sampaio <mateus4k@protonmail.ch>
2021-10-21 12:57:52 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker pull mateus4k/testing_app:latest
-docker stop app
-docker rm app
-docker run -p 80:3000 --name=app -d mateus4k/testing_app:latest
-docker image prune -a -f
+sudo docker pull mateus4k/testing_app:latest
+sudo docker stop app
+sudo docker rm app
+sudo docker run -p 80:3000 --name=app -d mateus4k/testing_app:latest
+sudo docker image prune -a -f
