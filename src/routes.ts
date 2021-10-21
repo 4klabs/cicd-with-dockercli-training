03ed74e11fb9bd7ea91f77b54028146deee6a34a
Mateus Sampaio <mateus4k@protonmail.ch>
2021-10-21 17:04:54 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Hello World!!!!!!!!!!!!!!!!!!!!' });
+  res.json({ message: 'Hello World' });
 });
 
 export default router;
